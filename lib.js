e062e3852cb1d278f90865fe081fdbe6d642e48b
Letladi <sletladi@gmail.com>
2023-11-02 08:44:05 +0200

    format with prettier

--- lib.js
+++ lib.js
@@ -1,27 +1,29 @@
-'use strict';
+"use strict";
 
-let fs = require('fs');
+let fs = require("fs");
 
-module.exports.Timestamper =  class Timestamper {
+module.exports.Timestamper = class Timestamper {
   timestamp(date) {
     date = date || new Date();
     let yr, month, day, hr, min, sec;
-    [ , yr, month, day, hr, min, sec] = date.toJSON().match(/(\d{4})-(\d+)-(\d+)T(\d+):(\d+):(\d+).*/);
-    return [yr, month, day, hr, min, sec].join('');
+    [, yr, month, day, hr, min, sec] = date
+      .toJSON()
+      .match(/(\d{4})-(\d+)-(\d+)T(\d+):(\d+):(\d+).*/);
+    return [yr, month, day, hr, min, sec].join("");
   }
 
   stampedNames(...fileNames) {
     if (fileNames.length === 1) {
       return `${this.timestamp()}_${fileNames[0]}`;
     } else {
-      fileNames = fileNames.map((fileName, i)=> {
-        return `${this.timestamp()}_${i+1}_${fileName}`;
+      fileNames = fileNames.map((fileName, i) => {
+        return `${this.timestamp()}_${i + 1}_${fileName}`;
       });
       return fileNames;
     }
   }
-}
+};
 
-module.exports.touch = (fileName)=> {
-  fs.writeFileSync(fileName, '');
+module.exports.touch = (fileName) => {
+  fs.writeFileSync(fileName, "");
 };
--- test.js
+++ test.js
@@ -1,49 +1,54 @@
-'use strict';
+"use strict";
 
-let lib = require('./lib'),
-    Timestamper = lib.Timestamper,
-    mocha = require('mocha'),
-    should = require('should'),
-    fs = require('fs');
+let lib = require("./lib"),
+  Timestamper = lib.Timestamper,
+  mocha = require("mocha"),
+  should = require("should"),
+  fs = require("fs");
 
-describe("Timestamper", ()=> {
-  describe("#timestamp", ()=> {
-    it('should return a timestamp of the given date', ()=> {
+describe("Timestamper", () => {
+  describe("#timestamp", () => {
+    it("should return a timestamp of the given date", () => {
       let timestamper = new Timestamper(),
-          date1 = new Date('2016-11-11T12:19:37.000Z'),
-          date2 = new Date('2016-05-11T12:19:37.000Z'),
-          stampString1 = '20161111121937',
-          stampString2 = '20160511121937';
+        date1 = new Date("2016-11-11T12:19:37.000Z"),
+        date2 = new Date("2016-05-11T12:19:37.000Z"),
+        stampString1 = "20161111121937",
+        stampString2 = "20160511121937";
       timestamper.timestamp(date1).should.equal(stampString1);
       timestamper.timestamp(date2).should.equal(stampString2);
     });
-    it('should return a timestamp of the current date if a timestamp is not given', ()=> {
+    it("should return a timestamp of the current date if a timestamp is not given", () => {
       let timestamp = new Timestamper();
-      timestamp.timestamp().should.be.an.instanceOf(String).and.have.lengthOf(14);
+      timestamp
+        .timestamp()
+        .should.be.an.instanceOf(String)
+        .and.have.lengthOf(14);
     });
   });
 
-  describe("#stampedNames", ()=> {
-    it('should return a file name with a timestamp', ()=> {
+  describe("#stampedNames", () => {
+    it("should return a file name with a timestamp", () => {
       let timestamper = new Timestamper(),
-        fileName = 'temp-file',
+        fileName = "temp-file",
         stampedName = timestamper.stampedNames(fileName);
-      (/^\d{14}\_\w+/.test(stampedName)).should.be.true();
+      /^\d{14}\_\w+/.test(stampedName).should.be.true();
     });
-    it('should return an array of timestamped file names if given more than one name', ()=> {
+    it("should return an array of timestamped file names if given more than one name", () => {
       let timestamper = new Timestamper(),
-          fileName1 = 'temp-file1',
-          fileName2 = 'temp-file2',
-          stampedNames = timestamper.stampedNames(fileName1, fileName2);
+        fileName1 = "temp-file1",
+        fileName2 = "temp-file2",
+        stampedNames = timestamper.stampedNames(fileName1, fileName2);
       stampedNames.should.be.an.instanceOf(Array);
-      (stampedNames.length).should.be.exactly(2);
+      stampedNames.length.should.be.exactly(2);
     });
-    it('should return an array of files names with a timestamp and sequence if given an array of names', ()=> {
+    it("should return an array of files names with a timestamp and sequence if given an array of names", () => {
       let timestamper = new Timestamper(),
-        fileNames = ['temp-file1', 'temp-file2'],
+        fileNames = ["temp-file1", "temp-file2"],
         stampedNames = timestamper.stampedNames(...fileNames);
-      stampedNames.forEach((stampedName, i)=> {
-        (new RegExp(`\\d{14}_${i+1}_${fileNames[i]}`)).test(stampedName).should.be.true();
+      stampedNames.forEach((stampedName, i) => {
+        new RegExp(`\\d{14}_${i + 1}_${fileNames[i]}`)
+          .test(stampedName)
+          .should.be.true();
       });
     });
   });
